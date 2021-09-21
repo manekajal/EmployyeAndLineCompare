2f8cd95154c886c769949c691ed4e43a2829f7b3
kajal <manekajal2014@gmail.com>
2021-09-21 17:22:19 +0530

    Welcome to Line Comparison Computation Program

---
+++ src/main/java/org/linecompare/DisplayMessage.java
@@ -0,0 +1,7 @@
+package org.linecompare;
+
+public class DisplayMessage {
+    public static void main(String[] args){
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
+}
